4ec9643f0bc7e1918ac5d7ab33d167ab5c11ea5b
leonpiedrahita <leondpr@gmail.com>
2021-09-07 19:26:36 -0500

    nuevaruta

--- src/store/index.js
+++ src/store/index.js
@@ -14,8 +14,9 @@ export default new Vuex.Store({
         ubicacion:'',
         icono:'',
         color:'',
-        ruta:'http://localhost:3000/',
+        /* ruta:'http://localhost:3000/', */
         /* ruta:'https://serene-inlet-85317.herokuapp.com/', */
+        ruta: 'https://sheltered-ravine-06791.herokuapp.com/',
        identificacion:"",
        ordenes:{},
        equipo:{},
